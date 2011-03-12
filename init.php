de8244d7ad9631b5758b3837c7f199bf70d24c3b
gimpe <gimpe@gimpe.com>
2011-03-12 15:20:49 -0500

    comments

--- init.php
+++ init.php
@@ -1,9 +1,24 @@
 <?php
 
 /**
- * @author gimpe
- * @link http://github.com/gimpe/kohana-doctrine
- * @license CC BY 3.0 (http://creativecommons.org/licenses/by/3.0/)
+ * kohana-doctrine initialization, executed by application bootstrap.php
+ *
+ * LICENSE: THE WORK (AS DEFINED BELOW) IS PROVIDED UNDER THE TERMS OF THIS
+ * CREATIVE COMMONS PUBLIC LICENSE ("CCPL" OR "LICENSE"). THE WORK IS PROTECTED
+ * BY COPYRIGHT AND/OR OTHER APPLICABLE LAW. ANY USE OF THE WORK OTHER THAN AS
+ * AUTHORIZED UNDER THIS LICENSE OR COPYRIGHT LAW IS PROHIBITED.
+ *
+ * BY EXERCISING ANY RIGHTS TO THE WORK PROVIDED HERE, YOU ACCEPT AND AGREE TO
+ * BE BOUND BY THE TERMS OF THIS LICENSE. TO THE EXTENT THIS LICENSE MAY BE
+ * CONSIDERED TO BE A CONTRACT, THE LICENSOR GRANTS YOU THE RIGHTS CONTAINED HERE
+ * IN CONSIDERATION OF YOUR ACCEPTANCE OF SUCH TERMS AND CONDITIONS.
+ *
+ * @category   module
+ * @package    kohana-doctrine
+ * @author     gimpe
+ * @copyright  2011 International Jaywalkers
+ * @license    http://creativecommons.org/licenses/by/3.0/ CC BY 3.0
+ * @link       http://github.com/gimpe/kohana-doctrine
  */
 
 // include kohana-doctrine config
@@ -12,18 +27,22 @@ $doctrine_config = Kohana::config('doctrine');
 // include Doctrine ClassLoader.php
 include $doctrine_config['doctrine_path'] . 'Doctrine/Common/ClassLoader.php';
 
+// defines Doctrine namespace
 $classLoader = new \Doctrine\Common\ClassLoader(
         'Doctrine', $doctrine_config['doctrine_path']);
 $classLoader->register();
 
+// defines Symfony namespace
 $classLoader = new \Doctrine\Common\ClassLoader(
         'Symfony', $doctrine_config['doctrine_path'] . '/Doctrine');
 $classLoader->register();
 
+// defines your "entitites" namespace
 $classLoader = new \Doctrine\Common\ClassLoader(
         $doctrine_config['entities_namespace'], $doctrine_config['entities_path']);
 $classLoader->register();
 
+// defines your "proxies" namespace
 $classLoader = new \Doctrine\Common\ClassLoader(
         $doctrine_config['proxies_namespace'], $doctrine_config['proxies_path']);
 $classLoader->register();
